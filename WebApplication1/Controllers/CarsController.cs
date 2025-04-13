db88ef819f8b46219490475c7dd776a92d894449
Макс Заславський <maksi43000@gmail.com>
2025-04-13 19:01:46 +0300

    Test hz

---
+++ WebApplication1/Controllers/CarsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+using WebApplication1.Data.interfaces;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class CarsController : Controller 
+    {
+        private readonly IAllCars _allCars;
+        private readonly ICarsCategory _allCategories;
+
+        public CarsController(IAllCars iAllCars, ICarsCategory ICarsCat)
+        {
+            _allCars = iAllCars;
+            _allCategories = ICarsCat;
+        }
+
+        public ViewResult List()
+        {
+            ViewBag.Title = "Page with cars";
+            CarsListViewModel obj = new CarsListViewModel();
+            obj.allCars = _allCars.Cars;
+            obj.currCategory = "Cars";
+
+            return View(obj);
+        }
+
+        
+    }
+}
---
+++ WebApplication1/ViewModels/CarsListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using WebApplication1.Data.Models;
+namespace WebApplication1.ViewModels
+{
+    public class CarsListViewModel
+    {
+        public IEnumerable<Car> allCars {  get; set; }
+
+        public string currCategory { get; set; }
+    }
+}
